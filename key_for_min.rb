6dcc00bdd7aaddb6dd1546e9ab7fbcd00abe2508
Lindsey Wells <lindsey.wells84@gmail.com>
2019-05-30 18:43:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,9 @@ def key_for_min_value(name_hash)
   lowest_value = nil
 
   name_hash.each do |key, value|
-    if lowest_value == nil || (value < lowest_value)
-      lowest_value == value
-      lowest_name == key
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_name = key
     end
   end
   lowest_name
